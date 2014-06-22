8085191265abc162c86eda66cf69bdf97f68f5be
Peter Nguyen <peter.hieu.nguyen@gmail.com>
2014-06-22 12:31:26 -0400

    Add fade and lerp util functions

---
+++ src/noise/utils/fade.rs
@@ -0,0 +1,5 @@
+/// C(2) continuous interpolant
+#[inline]
+pub fn fade(t: f64) -> f64 {
+  t * t * t * ( t * ( t * 6.0 - 15.0 ) + 10.0 )
+}
--- src/noise/utils/mod.rs
+++ src/noise/utils/mod.rs
@@ -3,7 +3,11 @@
 pub use utils::fastfloor::fastfloor;
 pub use utils::if_else::if_else;
 pub use utils::grad::{grad1, grad2, grad3};
+pub use utils::lerp::lerp;
+pub use utils::fade::fade;
 
 mod fastfloor;
 mod if_else;
 mod grad;
+mod lerp;
+mod fade;
